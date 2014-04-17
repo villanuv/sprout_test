fb8cd2b678a3cf11e639efdb492db5d2124c719d
Patrick Villanueva <Patrick@unknown0017f29958a9.att.net>
2014-04-17 17:16:46 -0500

    corrected db name

--- application/models/site_model.php
+++ application/models/site_model.php
@@ -4,20 +4,20 @@ class Site_model extends CI_model {
 
   function get_records() 
   {
-    $query = $this->db->get('entries');
+    $query = $this->db->get('url');
     return $query->result();
   }
 
   function get_record() 
   {
     $this->db->where('url', 'http://'.$this->uri->segment(3));
-    $query = $this->db->get('entries');
+    $query = $this->db->get('url');
     return $query->result();
   }
 
   function add_record($data)
   {
-    $this->db->insert('entries', $data);
+    $this->db->insert('url', $data);
     return;
   }
 
